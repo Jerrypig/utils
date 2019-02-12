c07d710de972c835e3aaa88c6ae4712a8be93c92
king feng <kingfeng_javaee@126.com>
2019-02-12 15:49:46 +0800

    other.js中关于url操作转移到url.js

--- js/url.js
+++ js/url.js
@@ -1,4 +1,4 @@
-class urlFn {
+class UrlFn {
 
     /**
      * 从URL中获取某个参数值
@@ -45,6 +45,45 @@ class urlFn {
         }
         else {
             return defaultValue;
+        }
+    }
+
+    /*获取全部url参数,并转换成json对象*/
+    getAllParams(url) {
+        var url = url ? url : window.location.href;
+        var _pa = url.substring(url.indexOf('?') + 1),
+            _arrS = _pa.split('&'),
+            _rs = {};
+        for (var i = 0, _len = _arrS.length; i < _len; i++) {
+            var pos = _arrS[i].indexOf('=');
+            if (pos == -1) {
+                continue;
+            }
+
+            var name = _arrS[i].substring(0, pos),
+                value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
+            _rs[name] = value;
+        }
+        return _rs;
+    }
+
+    /*删除url指定参数，返回url*/
+    delParams(url, name) {
+        var baseUrl = url.split('?')[0] + '?';
+        var query = url.split('?')[1];
+        if (query.indexOf(name) > -1) {
+            var obj = {};
+            var arr = query.split('&');
+            for (var i = 0; i < arr.length; i++) {
+                arr[i] = arr[i].split('=');
+                obj[arr[i][0]] = arr[i][1];
+            }
+            delete obj[name];
+            var url = baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g, '').replace(/\:/g, '=').replace(/\,/g, '&');
+            return url;
+        }
+        else {
+            return url;
         }
     }
 
